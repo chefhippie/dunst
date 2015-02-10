679f8485dcf1e51da39aae8def8fa301713fc516
Thomas Boerger <tboerger@suse.de>
2015-02-10 15:42:48 +0100

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: dunst
+# Attributes:: default
+#
+# Copyright 2013-2014, Thomas Boerger <thomas@webhippie.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["dunst"]["packages"] = %w(
+  dunst
+  libnotify-tools
+)
+
+default["dunst"]["zypper"]["enabled"] = true
+default["dunst"]["zypper"]["alias"] = "x11-windowmanagers"
+default["dunst"]["zypper"]["title"] = "X11 Windowmanagers"
+default["dunst"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/X11:/windowmanagers/openSUSE_#{node["platform_version"]}/"
+default["dunst"]["zypper"]["key"] = "#{node["dunst"]["zypper"]["repo"]}repodata/repomd.xml.key"
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name "dunst"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures dunst"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+depends "zypper"
+recipe "dunst", "Installs/Configures dunst"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"
